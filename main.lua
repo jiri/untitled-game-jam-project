221f5ce767ac984e3321905202a716acbee28d10
Ben Buckton <ben.buckton@factorio.com>
2019-11-10 14:59:47 +0100

    structure of mouse controller

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local renderer = require("renderer")
 local state_manager = require("state_manager")
+local mouse_cursor = require("mouse_cursor")
 
 local state
 
@@ -10,4 +11,12 @@ end
 
 function love.draw()
   renderer.render_state(state)
+end
+
+function love.mousepressed(x,y,button)
+  mouse_cursor.mousepressed(x,y,button)
+end
+
+function love.mousereleased(x,y,button)
+  mouse_cursor.mousereleased(x,y,button)
 end
---
+++ mouse_cursor.lua
@@ -0,0 +1,11 @@
+local cursor = {}
+
+cursor.mousepressed = function(x,y,button)
+
+end
+
+cursor.mousereleased = function(x,y,button)
+
+end
+
+return cursor
