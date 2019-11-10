615630b62f4db880e4d3fe9adfc74c930b15de39
Tom Mason <wheybags@wheybags.com>
2019-11-10 15:11:12 +0100

    fixed level list being backwards

--- level_data.lua
+++ level_data.lua
@@ -5,22 +5,22 @@ local level_data = {}
 level_data.levels =
 {
   {
-    enemies = {enemies.fire_boss},
-  },
-  {
-    enemies = {enemies.snowman},
-  },
-  {
-    enemies = {enemies.shield_sword_guy},
-  },
-  {
-    enemies = {enemies.metal_android},
+    enemies = {enemies.security_guard, enemies.security_guard, enemies.minion, enemies.minion, enemies.minion},
   },
   {
     enemies = {enemies.rocket_guy, enemies.minion, enemies.minion, enemies.minion, enemies.minion},
   },
   {
-    enemies = {enemies.security_guard, enemies.security_guard, enemies.minion, enemies.minion, enemies.minion},
+    enemies = {enemies.metal_android},
+  },
+  {
+    enemies = {enemies.shield_sword_guy},
+  },
+  {
+    enemies = {enemies.snowman},
+  },
+  {
+    enemies = {enemies.fire_boss},
   },
 }
 
