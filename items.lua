e650ee858a04d76ed908a2b6e4599f482bedcc66
Tom Mason <wheybags@wheybags.com>
2019-11-10 15:07:57 +0100

    add spites to the items we have sprites for

--- items.lua
+++ items.lua
@@ -19,22 +19,27 @@ local items =
   rocket_launcher = {
     id = "rocket_launcher",
     name = "Rocket Launcher",
+    sprite = love.graphics.newImage("gfx/bazooka.png"),
   },
   fire_bomb = {
     id = "fire_bomb",
     name = "Fire Bomb",
+    sprite = love.graphics.newImage("gfx/fire_bomb.png"),
   },
   tazer = {
     id = "tazer",
     name = "Tazer",
+    sprite = love.graphics.newImage("gfx/tazer.png"),
   },
   gun = {
     id = "gun",
     name = "Gun",
+    sprite = love.graphics.newImage("gfx/gun.png"),
   },
   spanner = {
     id = "spanner",
     name = "Spanner",
+    sprite = love.graphics.newImage("gfx/spanner.png"),
   },
   carrot = {
     id = "carrot",
